c3c2ffa5cf33d6c497b67fa77e397f66b85a9768
Lars-Dominik Braun <lars@6xq.net>
2017-01-31 09:39:14 +0100

    Fix Makefile

--- rtt.mk
+++ rtt.mk
@@ -1,3 +1,5 @@
-SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-RTT_SRC := $(wildcard *.c)
-RTT_INC := -I$(SELF_DIR)
+RTT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+RTT_SRC := $(wildcard $(RTT_DIR)/*.c)
+RTT_INC := -I$(RTT_DIR)
+RTT_VERSION := 612
+$(info Using RTT v$(RTT_VERSION) $(RTT_DIR))
